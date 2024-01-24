3c50a0c45b58cf4ceb5895641598d3fcd85ca03c
KacperOtreba <kacperotreba1@gmail.com>
2024-01-24 10:17:59 +0100

    stworzenie repo

---
+++ lekcja_2023.12.06/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace lekcja_2023._12._06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // System.Console.WriteLine(r7e(5));
+        // System.Console.WriteLine(Suma(34));
+        // System.Console.WriteLine(SumaR(56));
+        // System.Console.WriteLine(DecBin(23));
+        // System.Console.WriteLine(DecBinR(23));
+        // DecBinR2(23);
+        
+    }
+
+    static int r7d(int n){
+        if (n==1) return 2;
+        if (n==2) return 3;
+        if (n==3) return 4;
+        return r7d(n-1) - r7d(n-2) - r7d(n-3);
+    }
+
+    static int r7e(int n){
+        if (n == 1) return 1;
+        if (n==2) return 4;
+        if (n%2==0) return r7e(n-2)*4;
+        return r7e(n-2) + 2;
+    }
+
+    static int Suma(int liczba){
+        int suma = 0;
+        while (liczba > 0){
+            suma += liczba % 10;
+            liczba = liczba / 10;
+        }
+        return suma;
+    }
+
+    static int SumaR(int n){
+        if (n == 0) return 0;
+        return SumaR(n/10) + n%10;
+    }
+
+    static string DecBin(int n){
+        string bin = "";
+        while (n > 0){
+            bin = n%2 + bin;
+            n /= 2;
+        }
+        return bin;
+    }
+
+    static int DecBinR(int n){
+        if (n == 0) return 0;
+        return n % 2 + 10 * DecBinR(n/2);
+    }
+
+    static void DecBinR2(int n){
+        if (n==0) return;
+        DecBinR2(n/2);
+        System.Console.WriteLine(n%2);
+    }
+    
+}
---
+++ lekcja_2023.12.13/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Pipes;
+
+namespace lekcja_2023._12._13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1. Zapisz iteracyjnie i rekurencyjnie obliczanie sumy kodów ASCII wpisanego przez usera słowa
+        // string input = Console.ReadLine();
+        // int suma = 0;
+        // foreach (char item in input)
+        // {
+        //     suma += item;
+        // }
+        // System.Console.WriteLine(suma);
+        // System.Console.WriteLine(SumaASCII("AAA"));
+        // 2. Napisz iteracyjnie i rekurencyjnie obliczenie x^n (x do potęgi n)
+        System.Console.WriteLine(rekuodej(16,20));
+    }
+    public static int SumaASCII(string input){
+        // 1. Zapisz iteracyjnie i rekurencyjnie obliczanie sumy kodów ASCII wpisanego przez usera słowa
+        if (input == "") return 0;
+        return SumaASCII(input.Substring(1)) + (int)input[0];
+
+    }
+
+    public static int Potega(int x, int n){
+        if (n == 0) return 1;
+        return Potega(x, n-1*x);
+    }
+
+    public static void EuklidesOdej(int x, int y){
+        if (x == y){
+            System.Console.WriteLine(x);
+            return;
+        }     
+        if ( x > y) EuklidesOdej(x- y, y);
+        else EuklidesOdej(x, y-x);
+    }
+
+    static int OdejmowanieEuklidesa(int a, int b)
+    {
+        if (b == 0)
+        {
+            return a;
+        }
+        else
+        {
+            return OdejmowanieEuklidesa(b, a % b);
+        }
+    }
+}
